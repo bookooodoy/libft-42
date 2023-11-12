73e95459a5233b649a946e8d13c22118ba670915
bookooodoy <97317098+bookooodoy@users.noreply.github.com>
2023-11-12 18:01:42 +0100

    Update ft_itoa.c

--- fonctions_supplementaires/ft_itoa.c
+++ fonctions_supplementaires/ft_itoa.c
@@ -13,70 +13,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	count_digit_length(int n)
+int        count_digit_length(int n)
 {
-	int	length;
-
-	length = 0;
-	while (n /=10)
-		length++;
-	return (length);
+        int        length = 0;
+        while (n /=10)
+                length++;
+        return (length);
 }
 
-int	ft_strlen(char *s)
+int        ft_strlen(char *s)
 {
-	int	index;
+        int        index;
 
-	index = 0;
-	while (s[index])
-		index++;
-	return (index);
+        index = 0;
+        while (s[index])
+                index++;
+        return (index);
 }
 
-char	*reverse_char(char *s, int size)
+char        *reverse_char(char *s, int size)
 {
-	char	*reversed;
-	int	end;
-	int	index;
+        char        *reversed;
+        int        end;
+        int        index;
 
-	index = 0;
-	end = ft_strlen(s);
-	reversed = (char *)malloc(sizeof(char) * size);
-	while (s[index])
-	{
-		reversed[index] = s[end - index];
-		index++;
-	}
-	return (reversed);
+        index = 0;
+        end = ft_strlen(s) - 1;
+        reversed = (char *)malloc(sizeof(char) * size + 2);
+        while (s[index])
+        {
+                reversed[index] = s[end - index];
+                index++;
+        }
+        reversed[index] = '\0';
+        return (reversed);
 }
 
-char	*ft_itoa(int n)
+char        *ft_itoa(int n)
 {
-	int	dlen;
-	char	*itoa;
-	char	*start_num;
-	int	d;
+        int        dlen = count_digit_length(n);
+        char        *itoa;
+        char        *start_num;
+        int        d = 0;
 
-	d = 0;
-	dlen = count_digit_length(n);
-	start_num = (char *)malloc(sizeof(char) * dlen);
-	if (!(start_num))
-	{
-		start_num = NULL;
-		return (start_num);
-	}
-	while (dlen--)
-	{
-		start_num[d++] = (n % 10) + '0';
-		n /= 10;
-	}
-	itoa = reverse_char(start_num, d);
-	return (itoa);
+        start_num = (char *)malloc(sizeof(char) * dlen + 2);
+        if (!(start_num))
+        {
+                start_num = NULL;
+                return (start_num);
+        }
+        while (dlen >= 0)
+        {
+                start_num[d++] = (n % 10) + '0';
+                dlen--;
+                n /= 10;
+        }
+        start_num[d] = '\0';
+        itoa = reverse_char(start_num, d);
+        return (itoa);
 }
 
-int	main(void)
+int        main(void)
 {
-	char *itoa = ft_itoa(100);
-	printf("%s", itoa);
-	return 0;
+        char *itoa = ft_itoa(100);
+        printf("%s", itoa);
+        return 0;
 }
